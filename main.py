6ba87b1a6901c218838d214bda49a85d75135aa4
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-14 10:37:09 +0200

    321. Изменение цвета фона в игре

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 import sys
+from random import randint
+
+clock = pygame.time.Clock()
 
 pygame.init()
-pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("My Pygame")
+
+
 
 while True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+    screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+    # screen.fill(pygame.Color('yellow'))
+    # screen.fill((255, 0, 0))
+    pygame.display.update()
+    clock.tick(2)
