9c91da6981b059d9b4a88a8c55ab1885648bd173
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-18 11:46:07 +0200

    342. Добавление методов в класс Fighter

--- fighter.py
+++ fighter.py
@@ -8,3 +8,20 @@ class Fighter:
         self.x, self.y = SCREEN_WIDTH / 2 - self.width / 2, SCREEN_HEIGHT - self.height
         self.step = FIGHTER_STEP
         self.is_moving_left, self.is_moving_right = False, False
+
+    def move_left(self):
+        self.is_moving_left = True
+
+    def move_right(self):
+        self.is_moving_right = True
+
+    def stop_moving(self):
+        self.is_moving_left = False
+        self.is_moving_right = False
+
+    def update_position(self):
+        if self.is_moving_left and self.x >= self.step:  # процес перемещения влево
+            self.x -= self.step  # изменение координаты X для корабля
+
+        if self.is_moving_right and self.x <= SCREEN_WIDTH - self.width - self.step:
+            self.x += self.step
