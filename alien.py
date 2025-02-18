180156f3c3aaa476810fd76ea3e7fef730730177
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-18 14:10:25 +0200

    344. Добавление методов в класс Alien

--- alien.py
+++ alien.py
@@ -4,9 +4,21 @@ from random import randint
 
 class Alien:
     def __init__(self):
-        self.image = pygame.image.load('image/alien.png')
-        self.width, self.height = self.image.get_size()
+        self.image = pygame.image.load('image/alien.png') # добавление фото пришельца
+        self.width, self.height = self.image.get_size()  # получение размера фото
+        self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0  # получение случайных координат для появления пришельца
+        self.step = ALIEN_STEP  # начальная скорость пришельца
+        self.speed = self.step  # изменение скорости пришельца
+
+    def update_position(self):  # перемещение инопланетянина вниз
+        self.y +=self.speed
+
+    def increase_speed(self):  # увеличение скорости
+        self.speed += self.step / 2
+
+    def reset(self):  # сброс позиции и возврат инопланетянина в исходную позицию
+        self.increase_speed()
         self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0
-        self.step = ALIEN_STEP
-        self.speed = self.step
-        
+
+    def has_reached_fighter(self, fighter):  # метод проверяе достиг ли инопланетянин fighter
+        return self.y + self.height > fighter.y
