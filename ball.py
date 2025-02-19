21e958f5720d1de6d379ee083bdf3e894f500f4c
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-19 13:08:42 +0200

    346. Добавление методов в класс Ball

--- ball.py
+++ ball.py
@@ -7,5 +7,26 @@ class Ball:
         self.width, self.height = self.image.get_size()
         self.x, self.y = 0, 0
         self.step = BALL_STEP
-        self.was_fired = False
+        self.was_fired = False  # шар изначально невидим на экране
         self.fighter = fighter
+
+    def fire(self):
+        self.was_fired = True
+        self.x = self.fighter.x + self.fighter.width / 2 - self.width / 2
+        self.y = self.fighter.y - self.height
+
+    def update_position(self):  #  перемещение мяча вверх
+        if self.was_fired:
+            self.y -= self.step
+
+    def is_out_of_screen(self):  # проверка выхода мяча за границы экрана
+        return  self.y + self.height < 0
+
+    def reset(self):
+        self.was_fired = False
+
+    def is_collision(self, alien):  # есть ли столкновение или нет с alien
+        return  (
+            alien.x < self.x < alien.x + alien.width - self.width and
+            alien.y < self.y < alien.y + alien.height - self.height
+        )
